3aeb447d554d1529cce63472f781c561fb11a06d
gavirnenins-web <gavirnenins@gmail.com>
2026-05-28 03:38:36 +0000

    Added password validation

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('register.html')
-
 @app.route('/register', methods=['POST'])
 def register():
     name = request.form['name']
     email = request.form['email']
     password = request.form['password']
 
+    if len(password) < 6:
+        return "Password must be at least 6 characters!"
+
     with open('users.txt', 'a') as file:
         file.write(f"{name}, {email}, {password}\n")
 
     return f"User {name} registered successfully!"
-
-if __name__ == '__main__':
-    app.run(debug=True)
